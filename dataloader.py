aece8af10a456feb4a0b4acb76b94438bead1543
User <juancastro97125@gmail.com>
2020-04-15 11:03:38 -0500

    dataloader

--- dataloader.py
+++ dataloader.py
@@ -8,26 +8,7 @@ import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 import pdb 
-
-#pdb.set_trace()
-# For reading the csv_file with the annotations of the classes. 
-#amazon_classes = pd.read_csv('/home/jlcastillo/Proyecto/Understanding-The-Amazon/Dataset/train_classes.csv')
-# Esto devuelve algo asi: 
-        #train_0                                       haze primary
-        #train_1                    agriculture clear primary water
-        #train_2                                      clear primary
-#  Nombre de la imagen con su correspondiente clase.         
-
-def rename (csv_file):
-    csv_file = pd.read_csv('/home/jlcastillo/Proyecto/Understanding-The-Amazon/Dataset/train_classes.csv')
-    amazon_classes = csv_file
-    for i in len(amazon_classes):
-        name = amazon_classes.iloc[i,0]
-        new_name = name + '.jpg'
-        amazon_classes.iloc[i,0] = new_name
-    return amazon_classes    
-
-
+   
 
 
 
@@ -42,7 +23,7 @@ class AmazonDataset(Dataset):
                 on a sample.
         """
 
-        self.classes = pd.read_csv(csv_file)
+        self.classes = csv_file
         self.root_dir = root_dir
         self.transform = transform
 
@@ -50,6 +31,7 @@ class AmazonDataset(Dataset):
         return len(self.classes)
 
     def __getitem__(self,idx):
+
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
@@ -64,15 +46,34 @@ class AmazonDataset(Dataset):
     
             
 
-amazon_dataset = AmazonDataset(csv_file='/home/jlcastillo/Proyecto/Understanding-The-Amazon/Dataset/train_classes.csv',
+# For reading the csv_file with the annotations of the classes. 
+#amazon_classes = pd.read_csv('/home/jlcastillo/Proyecto/Understanding-The-Amazon/Dataset/train_classes.csv')
+# Esto devuelve algo asi: 
+        #train_0                                       haze primary
+        #train_1                    agriculture clear primary water
+        #train_2                                      clear primary
+#  Nombre de la imagen con su correspondiente clase.     
+
+def rename (csv_file):
+    file_name = pd.read_csv(csv_file)
+    amazon_classes = file_name
+    for i in range(len(amazon_classes)):
+        name = amazon_classes.iloc[i,0]
+        new_name = name + '.jpg'
+        amazon_classes.iloc[i,0] = new_name
+    return amazon_classes    
+
+
+amazon_dataset = AmazonDataset(csv_file = rename('/home/jlcastillo/Proyecto/Understanding-The-Amazon/Dataset/train_classes.csv'),
                                     root_dir='/home/jlcastillo/Proyecto/Understanding-The-Amazon/Dataset/train-jpg')
 
-fig = plt.figure()
+
+"""fig = plt.figure()
 
 for i in range(len(amazon_dataset)):
 
-    sample = amazon_dataset[i]+'.jpg' # TOCA ARREGLAR ESTO PORQUE SOLO LLEGA HASTA EL PEDAZO DE EL PATH/train_0 # No encuentra la imagen porque no tiene extensión .jpg
-
+    pdb.set_trace()
+    sample = amazon_dataset[i] 
     print(i, sample['image'].shape, sample['category'].shape)
 
     ax = plt.subplot(1, 4, i + 1)
@@ -82,7 +83,7 @@ for i in range(len(amazon_dataset)):
 
     if i == 3:
         plt.show()
-        break
+        break"""
 
 
 
