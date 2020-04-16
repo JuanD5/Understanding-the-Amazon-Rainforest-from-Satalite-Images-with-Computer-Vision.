6b0377f45fe5ec8004a9a5a9a279b0be022e1b57
User <juancastro97125@gmail.com>
2020-04-16 12:46:09 -0500

    csv_files

--- csv_creator.py
+++ csv_creator.py
@@ -19,19 +19,33 @@ data = []
 first_row = ['image_name','tags']
 data.append(first_row) 
 
-
-for i in range(len(dataframe)):
-    row = dataframe.iloc[i]
-    new_row = row.tolist()
-    data.append(new_row)
+datat = []  
+first_row = ['image_name','tags']
+datat.append(first_row) 
 
 
+num_total = len(dataframe)
+num_train = round(0.8*num_total)
 
+for i in range(num_total):
+    if i < num_train:
+       row = dataframe.iloc[i]
+       new_row = row.tolist()
+       data.append(new_row)            
+
+    else:
+        rowt = dataframe.iloc[i]
+        new_rowt = rowt.tolist()
+        datat.append(new_rowt) 
+
+        
 with open ('train.csv','w') as file:
     filewriter = csv.writer(file, delimiter=',')
     filewriter.writerows(data)                            
     
-
+with open ('test.csv','w') as file:
+    filewriter = csv.writer(file, delimiter=',')
+    filewriter.writerows(datat) 
     
 
 
